ffcf8e4c5a7295ef862698b8ed1f223b8e7871b5
hewe <272489473@qq.com>
2018-08-01 19:37:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# machine_learn
+机器学习常用算法
+# reference
+http://www.apachecn.org/
---
+++ app.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+import csv
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    print __file__
